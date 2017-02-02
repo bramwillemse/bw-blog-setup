a2c7dd326cdc648a3c16508a2eeffe2f58a77d5a
Bram Willemse <contact@bramwillemse.nl>
2017-02-02 15:08:49 +0100

    load autoload.php when needed

--- bramwillemsenl-setup.php
+++ bramwillemsenl-setup.php
@@ -2,7 +2,7 @@
 	Plugin Name: Bram Willemse Setup
 	Plugin URI: https://www.bramwillemse.nl
 	Description: Sets up custom WordPress architecture for Bram Willemse
-	Version: 0.4.1
+	Version: 0.4.2
 	Author: Bram Willemse
 	Author URI: http://www.bramwillemse.nl
 	Tested up to: 4.6.1
